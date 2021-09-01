0eba4e81b02ec359e205477977dd3b82d7ed0e26
Moe_Ghost <trofimchyk61082@gmail.com>
2021-09-01 13:53:39 +0300

    hw1

---
+++ hw1.js
@@ -0,0 +1,35 @@
+// №1
+var numbers = [ 254, 115, 78, 25, 91, 45, 37 ];
+
+for(var i = 0; i < numbers.length; i++) {
+	if(numbers[i] > 50) {
+		console.log(numbers[i]);
+	}
+}
+
+
+
+// №2
+var letters = [];
+var str = "Backend As A Service";
+var first_letter_str = "";
+
+str = str.split(' ');
+
+for(var i = 0; i < str.length; i++) {
+	letters.push(str[i][0]);
+}
+console.log(letters);
+
+for(i = 0; i <letters.length; i++) {
+	first_letter_str += letters[i];
+}
+
+console.log(first_letter_str);
+
+
+// №3
+
+function Data(number) {
+	if(typeof(number) === "number")
+}
