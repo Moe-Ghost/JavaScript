b51377a879d9cf57a91d6e8ef7373c88a87468f4
Moe_Ghost <trofimchyk61082@gmail.com>
2021-09-03 23:28:58 +0300

    js2

--- JS2/js2.js
+++ JS2/js2.js
@@ -1,11 +1,14 @@
-var nums = [4,3,2,7,11,15];
-var target = 9;
+var nums = [3, 2, 7, 11, 15];
+var target = 26;
 var result = []
-for (var i = 0 ; i < nums.length; i++) {
-	for (var j = i + 1; j <nums.lenth; j++) {
-		if(nums[i] + nums[j] == target) {
-			console.log("succses")
+for(let i = 0; i < nums.length; i++) {
+	for(let j = i + 1; j < nums.length; j++) {
+		if(nums[i] + nums[j] === target) {
+			result[0] = i;
+			result[1] = j;
+			break;
 		}
 	}
 }
-// console.log(result[0], "\t", retult[1])
+console.log(result);
+console.log("Because " + nums[result[0]] + " + " + nums[result[1]] +" == " + target + ", we return [" + result + "].")
